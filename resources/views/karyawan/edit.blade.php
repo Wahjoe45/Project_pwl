86a351376abcc8881deca6cf1888f99fed9080b2
Wahjoe45 <setiawanaryawahjoe@gmail.com>
2022-06-08 09:37:43 +0700

    Buat views untuk data karyawan

---
+++ resources/views/karyawan/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('template')
+
+@section('content')
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Edit Data Karyawan</h2>
+            </div>
+            <div class="float-right">
+                <a class="btn btn-secondary" href="{{ route('karyawan.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('karyawan.update',$karyawan->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIP:</strong>
+                <input type="text" name="NIP" class="form-control" placeholder="NIP Karyawan" value="{{ $karyawan->NIP }}">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama Karyawan:</strong>
+                <input type="text" name="NamaKaryawan" value="{{ $karyawan->NamaKaryawan }}" class="form-control" placeholder="NAMA KARYAWAN">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Asal:</strong>
+                <textarea class="form-control" style="height:150px" name="Asal" placeholder="Content">{{ $karyawan->Asal }}</textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Status:</strong>
+                <textarea class="form-control" style="height:150px" name="Status" placeholder="Content">{{ $karyawan->Status }}</textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+    </div>
+
+    </form>
+@endsection
