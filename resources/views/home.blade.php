dac91176c7456ef644977a8cd828677661b9a178
vinkharisma <vinkharismadzania@gmail.com>
2022-06-29 00:13:44 +0700

    edit title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,21 +10,21 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Title</h4>
+                        <h4>Welcome to Hospital Stisla Medical</h4>
                     </div>
                     <div class="card-body"></div>
                 </div>
             </div>
-            <div class="col-md-6">
+            {{-- <div class="col-md-6">
                 <div class="card">
                     <div class="card-header"></div>
                     <div class="card-body"></div>
                 </div>
-            </div>
-        </div> 
+            </div> --}}
+        </div>
         <div class="section-body">
 
         </div>
