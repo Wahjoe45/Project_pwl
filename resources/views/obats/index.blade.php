ec63d1301dfa89c47b0fff8545fc32feaed3dc66
vinkharisma <vinkharismadzania@gmail.com>
2022-06-30 08:32:55 +0700

    edit index pemesanan obat

--- resources/views/obats/index.blade.php
+++ resources/views/obats/index.blade.php
@@ -2,56 +2,155 @@
 
 @section('content')
 
-    <div class="row mt-5 mb-5">
-        <div class="col-lg-12 margin-tb">
-            <div class="float-left">
-                <h2>PEMESANAN OBAT</h2>
-            </div>
-            <div class="float-right">
-                <a class="btn btn-success" href="{{ route('obats.create') }}">Tambah Pesanan Obat</a>
+    <!-- Main Content -->
+
+    <section class="section">
+        <div class="section-header">
+            <h1>User List</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Components</a></div>
+                <div class="breadcrumb-item">Table</div>
             </div>
         </div>
-    </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
+        <div class="section-body">
+            <h2 class="section-title">User Management</h2>
+            <div class="row">
+                <div class="col-12">
+                    @include('layouts.alert')
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>Pemesanan Obat</h4>
+                            <div class="card-header-action">
+                                <a class="btn btn-icon icon-left btn-primary" href="{{ route('obats.create') }}">Create New
+                                    Obat</a>
+                                <a class="btn btn-info btn-primary active import">
+                                    <i class="fa fa-download" aria-hidden="true"></i>
+                                    Import Obat</a>
+                                <a class="btn btn-info btn-primary active" href="{{ route('user.export') }}">
+                                    <i class="fa fa-upload" aria-hidden="true"></i>
+                                    Export Obat</a>
+                                <a class="btn btn-info btn-primary active search">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                    Search Obat</a>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            {{-- <div class="show-import" style="display: none">
+                                <div class="custom-file">
+                                    <form action="{{ route('pgw.import') }}" method="post" enctype="multipart/form-data">
+                                        {{ csrf_field() }}
+                                        <label class="custom-file-label" for="file-upload">Choose File</label>
+                                        <input type="file" id="file-upload" class="custom-file-input" name="import_file">
+                                        <br/> <br/>
+                                        <div class="footer text-right">
+                                            <button class="btn btn-primary">Import File</button>
+                                        </div>s
+                                    </form>
+                                </div>
+                            </div> --}}
+
+                            <div class="show-search mb-3" style="display: none">
+                                <form id="search" method="GET" action="{{ route('obats.index') }}">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-4">
+                                            <label for="role">User</label>
+                                            <input type="text" name="name" class="form-control" id="name"
+                                                placeholder="User Name">
+                                        </div>
+                                    </div>
+                                    <div class="text-right">
+                                        <button class="btn btn-primary mr-1" type="submit">Submit</button>
+                                        <a class="btn btn-secondary" href="{{ route('obats.index') }}">Reset</a>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <th width="20px" class="text-center">No</th>
+                                            <th width="280px"class="text-center">Resep</th>
+                                            <th width="280px"class="text-center">Nama</th>
+                                            <th width="280px"class="text-center">Keterangan</th>
+                                            <th width="280px"class="text-center">Action</th>
+                                        </tr>
+
+                                        @foreach ($obats as $obat)
+
+                                            <tr>
+                                                <td>{{ ++$i }}</td>
+                                                <td><img src="/resep/{{ $obat->resep }}" width="100px"></td>
+                                                <td>{{ $obat->nama }}</td>
+                                                <td>{{ $obat->keterangan }}</td>
+
+                                                <td class="text-center">
+                                                    <div class="d-flex justify-content-center">
+                                                        <form action="{{ route('obats.destroy',$obat->id) }}" method="POST">
+                                                            <a class="btn btn-info" href="{{ route('obats.show',$obat->id) }}">Show</a>
+                                                            <a class="btn btn-primary" href="{{ route('obats.edit',$obat->id) }}">Edit</a>
+
+                                                            @csrf
+
+                                                            @method('DELETE')
+
+                                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                                <div class="d-flex justify-content-center">
+                                    {{ $obats->withQueryString()->links() }}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <table class="table table-bordered">
-
-        <tr>
-            <th width="20px" class="text-center">No</th>
-            <th width="280px"class="text-center">Resep</th>
-            <th width="280px"class="text-center">Nama</th>
-            <th width="280px"class="text-center">Keterangan</th>
-            <th width="280px"class="text-center">Action</th>
-        </tr>
-
-        @foreach ($obats as $obat)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td><img src="/resep/{{ $obat->resep }}" width="100px"></td>
-            <td>{{ $obat->nama }}</td>
-            <td>{{ $obat->keterangan }}</td>
-            <td>
-                <form action="{{ route('obats.destroy',$obat->id) }}" method="POST">
-                    <a class="btn btn-info" href="{{ route('obats.show',$obat->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('obats.edit',$obat->id) }}">Edit</a>
-
-                    @csrf
-
-                    @method('DELETE')
-
-                    <button type="submit" class="btn btn-danger">Delete</button>
-
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
-    {!! $obats->links() !!}
-
+        {{-- @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif --}}
+    </section>
 @endsection
+
+@push('customScript')
+    <script>
+        $(document).ready(function() {
+            $('.import').click(function(event) {
+                event.stopPropagation();
+                $(".show-import").slideToggle("fast");
+                $(".show-search").hide();
+            });
+            $('.search').click(function(event) {
+                event.stopPropagation();
+                $(".show-search").slideToggle("fast");
+                $(".show-import").hide();
+            });
+            //ganti label berdasarkan nama file
+            $('#file-upload').change(function() {
+                var i = $(this).prev('label').clone();
+                var file = $('#file-upload')[0].files[0].name;
+                $(this).prev('label').text(file);
+            });
+        });
+    </script>
+@endpush
+
+@push('customStyle')
+@endpush
